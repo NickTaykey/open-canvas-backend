6b3b0bd857d3ed2b6fb3be985caa691e6463af31
Niccolò Toccane <nico.toccane@gmail.com>
2021-01-06 18:50:03 +0100

    complete API

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -13,9 +11,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect('mongodb://localhost:27017/open-canvas', {
 	useNewUrlParser    : true,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+function raise404 (req, res, nxt) {
+	return res.status(404).json({
+		status  : 404,
+		message : 'Not Found'
+	});
+}
+
+router.get('*', raise404);
+router.post('*', raise404);
+router.put('*', raise404);
+router.delete('*', raise404);
 
 module.exports = router;
